c95dbc9a40c4b11a55303c0ac7da3e90c42dbb98
Nikhil kumar <Nikhil062022@gmail.com>
2025-06-11 11:51:19 +0530

    update metadata for GNJ website

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'GNJ ',
+  description: 'Cyber security website',
+  generator: 'Koncept solution',
 }
 
 export default function RootLayout({
